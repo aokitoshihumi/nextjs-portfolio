d79c69fac192936176edadda7009ee886874e59b
aokitoshihumi <mspw126315@gmail.com>
2025-07-31 18:03:47 +0900

    changeLayout

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -4,14 +4,15 @@ import rehypeRaw from "rehype-raw";
 import rehypeSanitize from "rehype-sanitize";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import "../../globals.css";
 
 type Props = {
-  params: Promise<{ id : string }>
-}
+  params: Promise<{ id: string }>;
+};
 
 const BlogDetailsPage = async ({ params }: Props) => {
   const { id } = await params;
-  
+
   //外部からの通信の時だけawaitが必要。
   const data = await client.get({
     endpoint: "blogs",
@@ -22,10 +23,11 @@ const BlogDetailsPage = async ({ params }: Props) => {
     <>
       <main className="p-10">
         <h1 className="text-3xl font-bold mb-4">{data.title}</h1>
-        <p className="text-amber-50 mb-4">{data.publishedAt}</p>
-        <ReactMarkdown rehypePlugins={[rehypeRaw, remarkGfm, rehypeSanitize]}>
-          {data.content}
-        </ReactMarkdown>
+        <div className="markdown text-center">
+          <ReactMarkdown rehypePlugins={[rehypeRaw, remarkGfm, rehypeSanitize]}>
+            {data.content}
+          </ReactMarkdown>
+        </div>
       </main>
     </>
   );
